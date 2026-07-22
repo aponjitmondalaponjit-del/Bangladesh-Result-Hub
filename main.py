7a72a98835ab80f1609337af15c3c054a5d5df4b
aponjitmondalaponjit-del <aponjitmondalaponjit@gmail.com>
2026-07-22 11:47:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
-import os
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
-
-TOKEN = os.environ.get("BOT_TOKEN")
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "🇧🇩 Bangladesh Result Hub (BRH)\n\n"
-        "Welcome!\n"
-        "The bot is currently under development."
-    )
-
 def main():
     app = Application.builder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    print("Bangladesh Result Hub (BRH) Started...")
-    app.run_polling(close_loop=False)
 
-if __name__ == "__main__":
-    main()
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bangladesh Result Hub (BRH) Started...")
+
+    app.run_polling()
