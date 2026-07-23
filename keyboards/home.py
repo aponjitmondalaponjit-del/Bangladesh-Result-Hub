6742b7f79cc99fdd4861adc0157facc37f320dd8
aponjitmondalaponjit-del <aponjitmondalaponjit@gmail.com>
2026-07-23 21:34:48 +0600

    Update home.py

--- keyboards/home.py
+++ keyboards/home.py
@@ -1,1 +1,16 @@
+from telegram import ReplyKeyboardMarkup
 
+
+def home_keyboard():
+    keyboard = [
+        ["📄 View Result"],
+        ["👤 My Profile", "👥 My Referrals"],
+        ["🎁 My Rewards", "📢 Notice"],
+        ["💬 Support"]
+    ]
+
+    return ReplyKeyboardMarkup(
+        keyboard,
+        resize_keyboard=True,
+        persistent=True
+    )
