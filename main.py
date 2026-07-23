8e79e80e02307178503dccc18f44d5d4ce971022
aponjitmondalaponjit-del <aponjitmondalaponjit@gmail.com>
2026-07-23 09:26:05 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import os
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        ["📘 SSC Result", "📗 HSC Result"],
+        ["🕌 Dakhil Result", "📙 Alim Result"],
+        ["👤 My Profile", "📢 Notice"],
+        ["💬 Support"]
+    ]
+
+    reply_markup = ReplyKeyboardMarkup(
+        keyboard,
+        resize_keyboard=True
+    )
+
     await update.message.reply_text(
-        "🇧🇩 Bangladesh Result Hub (BRH)\n\n"
-        "Welcome!\n"
-        "The bot is currently under development."
+        "🇧🇩 বাংলাদেশ রেজাল্ট হাব\n\n"
+        "স্বাগতম!\n\n"
+        "এই বটের মাধ্যমে আপনি বাংলাদেশ শিক্ষা বোর্ডের পরীক্ষার ফলাফল সহজেই দেখতে পারবেন।\n\n"
+        "নিচের একটি অপশন নির্বাচন করুন।",
+        reply_markup=reply_markup
     )
 
 def main():
