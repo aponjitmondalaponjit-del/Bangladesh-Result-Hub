6af50dd071e8e6c891eb4ef6f3e4c3f110a5fba8
aponjitmondalaponjit-del <aponjitmondalaponjit@gmail.com>
2026-07-26 09:15:51 +0600

    Add database module

--- database/database.py
+++ database/database.py
@@ -1,1 +1,42 @@
-# Database
+import json
+import os
+
+DATA_FILE = "user_data.json"
+
+
+def load_data():
+    if not os.path.exists(DATA_FILE):
+        return {}
+
+    with open(DATA_FILE, "r", encoding="utf-8") as f:
+        try:
+            return json.load(f)
+        except:
+            return {}
+
+
+def save_data(data):
+    with open(DATA_FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+def get_user(user_id):
+    data = load_data()
+    return data.get(str(user_id))
+
+
+def add_user(user_id, user_data):
+    data = load_data()
+    data[str(user_id)] = user_data
+    save_data(data)
+
+
+def update_user(user_id, user_data):
+    data = load_data()
+    data[str(user_id)] = user_data
+    save_data(data)
+
+
+def user_exists(user_id):
+    data = load_data()
+    return str(user_id) in data
