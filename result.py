8b17aec720764e828edf9ac3ef9f40aa6621d008
aponjitmondalaponjit-del <aponjitmondalaponjit@gmail.com>
2026-07-26 09:09:12 +0600

    Add result module

--- result.py
+++ result.py
@@ -1,1 +1,10 @@
-{}
+def get_result_menu():
+    return (
+        "📄 View Result\n\n"
+        "নিচের একটি পরীক্ষা নির্বাচন করুন।\n\n"
+        "📘 SSC Result\n"
+        "📗 HSC Result\n"
+        "🕌 Dakhil Result\n"
+        "📙 Alim Result\n\n"
+        "🚧 এই অংশের কাজ চলছে।"
+    )
