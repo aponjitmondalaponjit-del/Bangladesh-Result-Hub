1cd8bc2d998160286dfc3eadfee9f7f5df076062
aponjitmondalaponjit-del <aponjitmondalaponjit@gmail.com>
2026-07-29 23:29:51 +0600

    Add users database

---
+++ database/users.py
@@ -0,0 +1,25 @@
+USERS = {}
+
+
+def get_user(user_id):
+    return USERS.get(user_id)
+
+
+def create_user(user_id):
+    if user_id not in USERS:
+        USERS[user_id] = {
+            "id": user_id,
+            "language": "বাংলা",
+            "premium": False,
+            "referrals": 0,
+        }
+
+    return USERS[user_id]
+
+
+def save_user(user):
+    USERS[user["id"]] = user
+
+
+def all_users():
+    return USERS.values()
