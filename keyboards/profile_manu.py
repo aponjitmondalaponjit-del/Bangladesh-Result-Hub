6f8585d20f8af1b1cf79d1a52bd4fbc4318077eb
aponjitmondalaponjit-del <aponjitmondalaponjit@gmail.com>
2026-07-30 16:15:51 +0600

    Add profile menu keyboard

---
+++ keyboards/profile_manu.py
@@ -0,0 +1,9 @@
+def profile_menu():
+    return [
+        ["👤 আমার প্রোফাইল"],
+        ["📄 Save My Roll"],
+        ["👥 Referral"],
+        ["🎁 Rewards"],
+        ["💎 Premium"],
+        ["🔙 Main Menu"],
+    ]
