e272ac19aeb635a92f0dd9cd732869cb7b8fad51
aponjitmondalaponjit-del <aponjitmondalaponjit@gmail.com>
2026-07-31 12:49:30 +0600

    Add education board provider

---
+++ providers/educationboard.py
@@ -0,0 +1,11 @@
+def get_education_result(
+    exam,
+    board,
+    roll,
+    registration,
+):
+    return {
+        "success": False,
+        "provider": "education",
+        "message": "Not implemented yet.",
+    }
