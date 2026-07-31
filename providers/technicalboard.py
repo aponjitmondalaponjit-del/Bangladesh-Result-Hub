e6cfb0e2768521fea3a59be55f0f0689c3384b20
aponjitmondalaponjit-del <aponjitmondalaponjit@gmail.com>
2026-07-31 12:53:22 +0600

    Add technical board provider

---
+++ providers/technicalboard.py
@@ -0,0 +1,11 @@
+def get_technical_result(
+    exam,
+    board,
+    roll,
+    registration,
+):
+    return {
+        "success": False,
+        "provider": "technical",
+        "message": "Not implemented yet.",
+    }
