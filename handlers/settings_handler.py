a5df34a81b1aad8bad771ccfbca486eaebf85280
aponjitmondalaponjit-del <aponjitmondalaponjit@gmail.com>
2026-07-31 10:31:17 +0600

    Create settings handler

--- handlers/settings_handler.py
+++ handlers/settings_handler.py
@@ -1,11 +1,14 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from keyboards.settings import settings_keyboard
+from keyboards.settings_menu import settings_menu
 
 
-async def settings_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def settings_handler(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE,
+):
     await update.message.reply_text(
         "⚙️ Settings",
-        reply_markup=settings_keyboard()
+        reply_markup=settings_menu(),
     )
