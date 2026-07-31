071c13064e21d107eb7af4b04d2276174aa8a08b
aponjitmondalaponjit-del <aponjitmondalaponjit@gmail.com>
2026-07-31 12:51:08 +0600

    Add madrasa board provider

---
+++ providers/madrasaboard.py
@@ -0,0 +1,11 @@
+def get_madrasa_result(
+    exam,
+    board,
+    roll,
+    registration,
+):
+    return {
+        "success": False,
+        "provider": "madrasa",
+        "message": "Not implemented yet.",
+    }
