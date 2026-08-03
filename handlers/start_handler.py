790a29ac9bd8295cd2c63481083dc2dd51470f36
aponjitmondalaponjit-del <aponjitmondalaponjit@gmail.com>
2026-08-03 11:32:38 +0600

    feat: improve start handler and result menu keyboard

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,13 +1,32 @@
+"""
+==========================================================
+Bangladesh Result Hub (BRH)
+
+Start Handler
+
+Version : 1.0.0
+
+Responsibilities:
+- Handle /start command
+- Show Main Menu
+==========================================================
+"""
+
 from telegram import Update
 from telegram.ext import ContextTypes
 
 from keyboards.main_menu import main_menu
 
+
 async def start_handler(
     update: Update,
     context: ContextTypes.DEFAULT_TYPE,
 ):
+    """
+    Handle /start command.
+    """
+
     await update.message.reply_text(
-        "🇧🇩 বাংলাদেশ রেজাল্ট হাবে স্বাগতম",
+        text="🇧🇩 বাংলাদেশ রেজাল্ট হাবে স্বাগতম",
         reply_markup=main_menu(),
     )
