e99e96bb4ab61dc826b1f323a23e26f02c225447
aponjitmondalaponjit-del <aponjitmondalaponjit@gmail.com>
2026-08-07 20:31:36 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,21 @@
-"""
-==========================================================
-Bangladesh Result Hub (BRH)
-
-Main Application
-
-Version : 1.0.0
-
-Responsibilities:
-- Start Telegram Bot
-- Load configuration
-- Register handlers
-- Run polling
-==========================================================
-"""
-
 from telegram.ext import Application
 
 from config import BOT_TOKEN
 from handlers.handler_registration import register_handlers
 
 
-def main() -> None:
-    """
-    Start BRH Bot.
-    """
-
-    app = Application.builder().token(
-        BOT_TOKEN
-    ).build()
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
 
     register_handlers(app)
 
     print("🇧🇩 Bangladesh Result Hub Bot Started")
 
-    app.run_polling()
+    try:
+        app.run_polling()
+    except Exception as e:
+        print(f"BOT ERROR: {e}")
+        raise
 
 
 if __name__ == "__main__":
