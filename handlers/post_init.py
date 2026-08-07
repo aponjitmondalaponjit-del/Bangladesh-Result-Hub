23db0f43faff45cfe3d0eb4d115324bfb2bf06b0
aponjitmondalaponjit-del <aponjitmondalaponjit@gmail.com>
2026-08-07 23:33:39 +0600

    Add Telegram side menu configuration

--- handlers/post_init.py
+++ handlers/post_init.py
@@ -1,9 +1,16 @@
-from telegram import BotCommand
+from telegram import BotCommand, MenuButtonCommands
+from telegram.ext import Application
 
 
-async def post_init(application):
-    await application.bot.set_my_commands([
+async def post_init(app: Application):
+
+    await app.bot.set_my_commands([
+        BotCommand("start", "🏠 Main Menu"),
         BotCommand("settings", "⚙️ Settings"),
         BotCommand("support", "🆘 Support"),
         BotCommand("premium", "💎 Premium"),
     ])
+
+    await app.bot.set_chat_menu_button(
+        menu_button=MenuButtonCommands()
+    )
