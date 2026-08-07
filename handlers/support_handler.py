9a72d4ea8a9b613e5bd668e8626e5aeecfbf060b
aponjitmondalaponjit-del <aponjitmondalaponjit@gmail.com>
2026-08-07 23:14:20 +0600

    Update support_handler.py

--- handlers/support_handler.py
+++ handlers/support_handler.py
@@ -1,10 +1,8 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from support import get_support_message
-
 
 async def support_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        get_support_message()
+        "🆘 Support\nখুব শীগ্রই চালু হবে.."
     )
