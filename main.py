372518b090748b94028fd913eb0faeb6e1279afc
aponjitmondalaponjit-del <aponjitmondalaponjit@gmail.com>
2026-08-07 23:24:44 +0600

    Add side menu handlers and post initialization

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
+"""
+==========================================================
+Bangladesh Result Hub (BRH)
+
+Main Application
+
+Version : 1.0.0
+
+Responsibilities:
+- Start Telegram Bot
+- Load configuration
+- Register handlers
+- Set Telegram Menu Button
+==========================================================
+"""
+
 from telegram.ext import Application
 
 from config import BOT_TOKEN
 from handlers.handler_registration import register_handlers
+from handlers.post_init import post_init
 
 
 def main():
-    app = Application.builder().token(BOT_TOKEN).build()
+    app = (
+        Application.builder()
+        .token(BOT_TOKEN)
+        .post_init(post_init)
+        .build()
+    )
 
     register_handlers(app)
 
